606aea51b77c2b243e190bd4d3d2f74fbd2cdb06
syed farhan sr <syedfarhan17861@gmail.com>
2024-01-09 10:12:58 +0530

    index database update

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
-require "functions.php";
-require "router.php";
+require 'functions.php';
+//require 'router.php';
+require 'Database.php';
+
+$db = new Database();
+$post = $db->query("select * from post")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($post);
