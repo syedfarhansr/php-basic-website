86d4e5c303f246219ac01b67048506a66cd78115
syed farhan sr <syedfarhan17861@gmail.com>
2024-01-09 10:12:39 +0530

    function update

--- functions.php
+++ functions.php
@@ -1,19 +1,14 @@
 <?php
- 
- function dd($value) 
- {
+
+function dd($value)
+{
     echo "<pre>";
     var_dump($value);
-    echo"</pre>";
+    echo "</pre>";
 
     die();
-    }
-    function UrLIs($value) {
-         return $_SERVER['REQUEST_URI'] === $value;
-    }
-    
+}
 
-
-
-
-    
+function urlIs($value) {
+    return $_SERVER['REQUEST_URI'] === $value;
+}
