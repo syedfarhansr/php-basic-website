34f4867370e3c0ac7a1f116d0680e3d3c6c8508e
syed farhan sr <syedfarhan17861@gmail.com>
2024-01-16 22:32:57 +0530

    index

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
 require 'functions.php';
-//require 'router.php';
 require 'Database.php';
-
-$db = new Database();
-$post = $db->query("select * from post")->fetchAll(PDO::FETCH_ASSOC);
-
-dd($post);
+require 'Response.php';
+require 'router.php';
